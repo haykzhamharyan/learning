2d427edf32d7a43f4ea226ac6cf862735434807a
Hayk Zhamharyan <hayk.zhamharyan@synisys.com>
2022-05-23 21:46:19 +0400

    Fixed bash syntax

--- myfirstbash.sh
+++ myfirstbash.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 AGE=16
-echo "What is your name:"
-read -p name
-echo "How old are you?"
-read -p AGE
+#echo "What is your name:"
+read -p "What is your name" name
+#echo "How old are you?"
+read -p "How old are you" AGE
 
 echo "Your name is $name and you are $AGE years old."
